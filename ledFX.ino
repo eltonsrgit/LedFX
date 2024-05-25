9966aa8281962f7281c8b18dbe0f280ee3999102
eltonsrgit <167440350+eltonsrgit@users.noreply.github.com>
2024-05-25 17:02:32 -0300

    ledFX.ino

--- ledFX.ino
+++ ledFX.ino
@@ -1,67 +1,93 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 2 //pino do anel de leds
+#define PIN 6 // pino do anel de leds
 #define NUMPIXELS 8 // quantidade de leds do anel
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // necessario
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // necessário
 
 void setup(){
   pixels.begin(); // inicializa o objeto pixels
-  pixels.setBrightness(100); // brilho de 0 até 255
+  pixels.setBrightness(30); // brilho de 0 até 255
   pixels.clear(); // "limpa" tudo, apaga todos os leds
-  ledLight(0, 0, 0); 
+  ledLight(0, 0, 0);
 }
 
 void loop(){ // combine os valores r, g, b, para obter novas cores!
-  ledBlink(150, 0, 0, 700);
+  ledPatrol(150, 0, 0, 0, 0, 150, 50);
+  //ledBlink(0, 150, 0, 700);
   //ledLight(0, 150, 0);
   //ledCircle(0, 0, 150, 500);
   //ledCircleBlink(150, 0, 150, 700);
 }
 
 
-void ledBlink(int r, int g, int b, int time) {    // pisca todas os leds em um intervalo de tempo
+void ledBlink(uint8_t r, uint8_t g, uint8_t b, int time) { // pisca todas os leds em um intervalo de tempo
   pixels.clear();
-  for(int i=0; i<NUMPIXELS; i++) { 
+  for(uint8_t i = 0; i < NUMPIXELS; i++) { 
     pixels.setPixelColor(i, pixels.Color(r, g, b));
     pixels.show();   
   }
   pixels.clear();
   delay(time);
-  for(int i=0; i<NUMPIXELS; i++) { 
+  for(uint8_t i = 0; i < NUMPIXELS; i++) { 
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
     pixels.show();    
   }
   delay(time);
 }
+
 /*----------------------------------------------------------------------------------------*/
-void ledLight (int r, int g, int b) {   // luz contínua
+void ledLight (uint8_t r, uint8_t g, uint8_t b) { // luz contínua
   pixels.clear();
-  for(int i=0; i<NUMPIXELS; i++) { 
+  for(uint8_t i = 0; i < NUMPIXELS; i++) { 
     pixels.setPixelColor(i, pixels.Color(r, g, b));
     pixels.show();    
   }
 }
+
 /*----------------------------------------------------------------------------------------*/
-void ledCircle(int r, int g, int b, int time) {   // luzes "andam" em círculo numa certa velocidade dependendo do tempo
+void ledCircle(uint8_t r, uint8_t g, uint8_t b, int time) { // luzes "andam" em círculo numa certa velocidade dependendo do tempo
   pixels.clear();
-  for(int i=0; i<NUMPIXELS; i++) { 
+  for(uint8_t i = 0; i < NUMPIXELS; i++) { 
     pixels.setPixelColor(i, pixels.Color(r, g, b));
     pixels.show();    
     delay(time);
     pixels.clear(); 
   }
 }
+
 /*----------------------------------------------------------------------------------------*/
-void ledCircleBlink(int r, int g, int b, int time) {  // luzes "andam" em círculo e piscam no final numa certa velocidade dependendo do tempo
+void ledCircleBlink(uint8_t r, uint8_t g, uint8_t b, int time) { // luzes "andam" em círculo e piscam no final numa certa velocidade dependendo do tempo
   pixels.clear();
-  for(int i=0; i<NUMPIXELS; i++) { 
+  for(uint8_t i = 0; i < NUMPIXELS; i++) { 
     pixels.setPixelColor(i, pixels.Color(r, g, b));
     pixels.show();    
   }
-  for(int i=0; i<NUMPIXELS; i++) { 
+  for(uint8_t i = 0; i < NUMPIXELS; i++) { 
     pixels.setPixelColor(i, pixels.Color(r, g, b));
     pixels.show();
     delay(time);
     pixels.clear();
   }
+}
+
+
+void ledPatrol(uint8_t r, uint8_t g, uint8_t b, uint8_t r2, uint8_t g2, uint8_t b2, int time) { // luzes "andam" em círculo e piscam no final numa certa velocidade dependendo do tempo
+  pixels.clear();
+  for(int i = 0; i < NUMPIXELS; i++) { 
+    pixels.clear();
+    pixels.setPixelColor(i, pixels.Color(r, g, b));
+    pixels.show();
+    delay(time);
+  }
+  for(int i = NUMPIXELS; i > 0; i--) { 
+    pixels.clear();
+    pixels.setPixelColor(i, pixels.Color(r2, g2, b2));
+    pixels.show();
+    delay(time);
+  }
+  for(int i = 0; i < NUMPIXELS; i++){
+    pixels.setPixelColor(i+1, pixels.Color(150, 150, 150));
+    pixels.show();
+  }
+  delay(time);
 }
